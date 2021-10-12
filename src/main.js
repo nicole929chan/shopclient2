eba94d5636c1d1ae265a92a2d606bd5ceeca9225
Nicole <nicole929chan@gmail.com>
2021-10-12 15:38:36 +0800

    vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
   store,
-  router,
+  vuetify,
+  router
 }).$mount('#app');
